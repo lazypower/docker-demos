2a5ed78cf88625df0f51ad5a75c3664914abadbd
Charles Butler <chuck@dasroot.net>
2015-06-05 02:02:25 -0400

    Adds a mostly functional twitter sentiment analysis stack

---
+++ twitter-sentiment-analysis/README.md
@@ -0,0 +1,7 @@
+## Twitter Sentiment Analysis 
+
+This formation is based off of this excellent blog post by realpython.com
+
+https://realpython.com/blog/python/twitter-sentiment-python-docker-elasticsearch-kibana/
+
+
---
+++ twitter-sentiment-analysis/streaming/app.py
@@ -0,0 +1,75 @@
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
+from os import getenv
+
+# import twitter keys and tokens
+from config import *
+
+# create instance of elasticsearch
+es = Elasticsearch(['elasticsearch:9200'])
+
+
+class TweetStreamListener(StreamListener):
+
+    # on success
+    def on_data(self, data):
+
+        # decode json
+        dict_data = json.loads(data)
+
+        # pass tweet into TextBlob
+        tweet = TextBlob(dict_data["text"])
+
+        # output sentiment polarity
+        print tweet.sentiment.polarity
+
+        # determine if sentiment is positive, negative, or neutral
+        if tweet.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif tweet.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+
+        # output sentiment
+        print sentiment
+
+        # add text and sentiment info to elasticsearch
+        es.index(index="sentiment",
+                 doc_type="test-type",
+                 body={"author": dict_data["user"]["screen_name"],
+                       "date": dict_data["created_at"],
+                       "message": dict_data["text"],
+                       "polarity": tweet.sentiment.polarity,
+                       "subjectivity": tweet.sentiment.subjectivity,
+                       "sentiment": sentiment})
+        return True
+
+    # on failure
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+    print "Started"
+    # create instance of the tweepy tweet stream listener
+    listener = TweetStreamListener()
+
+    # set twitter keys/tokens
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    print "Streaming"
+    # create instance of the tweepy stream
+    stream = Stream(auth, listener)
+
+    keyword = getenv('KEYWORD')
+
+    if not keyword:
+        raise ValueError("No keyword provided, set KEYWORD Env Var")
+
+    # search twitter for keyword
+    stream.filter(track=[keyword])
